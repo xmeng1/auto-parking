cf9299ae5d63c749c4593f934fd90b2691f9153b
Xin Meng <mengxin.city@gmail.com>
2019-08-14 22:10:23 +0100

    update pom and junit test

---
+++ src/test/java/science/mengxin/java/auto_parking/AutoParkingAppTests.java
@@ -0,0 +1,20 @@
+package science.mengxin.java.auto_parking;
+
+import com.google.common.base.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoParkingAppTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
