388c9ea2fb3217bf5df202c82867a7cb7f5dc2e0
Xin Meng <mengxin.city@gmail.com>
2019-08-17 09:02:12 +0100

    add new method to ignore illegal char of command and test

--- src/main/java/science/mengxin/java/auto_parking/utilities/CarParkUtils.java
+++ src/main/java/science/mengxin/java/auto_parking/utilities/CarParkUtils.java
@@ -6,6 +6,7 @@ import science.mengxin.java.auto_parking.model.Command;
 import science.mengxin.java.auto_parking.service.AutoParkingServiceImpl;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,6 +14,13 @@ import java.util.stream.Stream;
 public class CarParkUtils {
     private static final Logger logger = LoggerFactory.getLogger(AutoParkingServiceImpl.class);
 
+
+    /**
+     * Convert the String of car park commands to Enum list. If any of command char is illegal, return empty
+     *
+     * @param commandListStr string of commands like: "FFLFFLFFLFF"
+     * @return  if all char of commands are validated, return List of Command, or return Optional.Empty
+     */
     static public Optional<List<Command>> strToCommandList(String commandListStr) {
         Stream<Character> commandCharStream = commandListStr.chars()
                 .mapToObj(c -> (char) c);
@@ -27,19 +35,23 @@ public class CarParkUtils {
         }
     }
 
+    /**
+     * Convert the String of car park commands to Enum list. If any of command char is illegal, ignore it.
+     * @param commandListStr string of commands like: "FFLFFLFFLFF"
+     * @return if all char of commands are validated, return List of Command, or ignore any illegal char of command.
+     */
     static public Optional<List<Command>> strToCommandListIgnoreUnknown(String commandListStr) {
         Stream<Character> commandCharStream = commandListStr.chars()
                 .mapToObj(c -> (char) c);
         return Optional.of(commandCharStream.map(x -> {
                     try {
-
                         return Command.valueOf(x.toString());
                     } catch (Exception e) {
-                        logger.error("The String of command list [{}] cannot convert to command list because of [{}] cannot be recognised. Exception: {}",
+                        logger.warn("The String of command list [{}] cannot convert to command list because of [{}] cannot be recognised. Exception: {}",
                                 commandListStr, x, e.getMessage());
                         return null;
                     }
                 }
-        ).collect(Collectors.toList()));
+        ).filter(Objects::nonNull).collect(Collectors.toList()));
     }
 }
--- src/test/groovy/science/mengxin/java/auto_parking/utilities/CarParkUtilsTest.groovy
+++ src/test/groovy/science/mengxin/java/auto_parking/utilities/CarParkUtilsTest.groovy
@@ -23,6 +23,9 @@ class CarParkUtilsTest extends Specification {
     @Shared
     def RFL = of([R, F, L])
     @Shared
+    def RL = of([R, L])
+
+    @Shared
     def RFLFRFLF = of([R, F, L, F, R, F, L, F])
     @Shared
     def FFLFFLFFLFF = of([F, F, L, F, F, L, F, F, L, F, F])
@@ -42,11 +45,34 @@ convert the commands string : #commandsStr to Command List and result #expectedR
         then: "result should be as expected"
         result == expectedResult
         where: "the scenarios list"
-        commandsStr   || expectedResult | description
-        "RFL"         || RFL            | "accepted string"
-        "RFLFRFLF"    || RFLFRFLF       | "accepted string"
-        "FFLFFLFFLFF" || FFLFFLFFLFF    | "accepted string"
-        "FLFLFFRFFF"  || FLFLFFRFFF     | "accepted string"
-        "RXL"         || EMPTY          | "unknown X"
+        commandsStr                    || expectedResult | description
+        "RFL"                          || RFL            | "accepted string"
+        "RFLFRFLF"                     || RFLFRFLF       | "accepted string"
+        "FFLFFLFFLFF"                  || FFLFFLFFLFF    | "accepted string"
+        "FLFLFFRFFF"                   || FLFLFFRFFF     | "accepted string"
+        "RXL"                          || EMPTY          | "unknown X"
+        "sdf123sdxxxl!\"£!\"*()&^%|\$" || EMPTY          | "unknown X"
+    }
+
+
+    @Unroll("""
+convert the commands string : #commandsStr to Command List and result #expectedResult
+""")
+    def "StrToCommandListIgnoreUnknow"() {
+        given: ""
+
+        when: "convert the string to command list"
+        def result = CarParkUtils.strToCommandListIgnoreUnknown(commandsStr)
+        then: "result should be as expected"
+        result == expectedResult
+        where: "the scenarios list"
+        commandsStr                              || expectedResult | description
+        "RFL"                                    || RFL            | "accepted string"
+        "RFLFRFLF"                               || RFLFRFLF       | "accepted string"
+        "FFLFFLFFLFF"                            || FFLFFLFFLFF    | "accepted string"
+        "FLFLFFRFFF"                             || FLFLFFRFFF     | "accepted string"
+        "FLAFBLCFDFERFGFKKKF"                    || FLFLFFRFFF     | "ignore any"
+        "RXL"                                    || RL             | "ignore X"
+        "F1L23sdxxFLxl!\"£FFRF!\"*FF()&^%|\$" || FLFLFFRFFF     | "ignore any"
     }
 }
